7593d66525400c92c86486d05ed8444d659c19b0
afa <bla>
2024-12-14 20:07:56 +0100

    vimtex removed autocommand

--- lua/main/vimtex.lua
+++ lua/main/vimtex.lua
@@ -1,20 +1,8 @@
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = {"*.md", "*.tex"},
-    callback = function()
-        local status_ok, which_key = pcall(require, "which-key")
-        if not status_ok then
-            return
-        end
-        which_key.register {
-                ["<leader>Ll"] = { "<plug>(vimtex-compile)", "Start latex compile" },
-        }
-    end,
-})
-
 local g = vim.g
 
 g.tex_flavor='latex'
---g.vimtex_view_method='zathura'
+--g.vimtex_view_method='zathura'a
+g.vimtex_fold_enabled=0
 g.vimtex_quickfix_mode=0
 g.tex_conceal='abdmg'
-vim.api.nvim_set_option("conceallevel", 1)
+vim.api.nvim_set_option_value("conceallevel", 1, {})
