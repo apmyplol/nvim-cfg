1872141d648f781dd6f23c28d26157ac921d337e
afa <bla>
2026-08-11 19:29:45 +0200

    fixed stuff

--- lua/main/toggleterm.lua
+++ lua/main/toggleterm.lua
@@ -38,24 +38,14 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+
+local ncdu = Terminal:new({ cmd = "node", hidden = true })
+
+function _NCDU_TOGGLE()
+	ncdu:toggle()
 end
 
-
--- local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
---
--- function _NCDU_TOGGLE()
--- 	ncdu:toggle()
--- end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-	htop:toggle()
-end
 
 local python = Terminal:new({ cmd = "python", hidden = true })
 
